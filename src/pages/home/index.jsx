6fdd8e78cc75741771ebd7e6751e4a0af775c3b6
Medita_Knorr <Mediimarvel@Hotmail.com>
2021-03-30 21:32:34 +0200

    Remodelando o projecto
    Modificando ficheiros na pasta SRC

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../../styles/global.css'
+
+export default function Index() {
+    return (
+        <React.Fragment>
+            <header className={() => {return}}>
+
+            </header>
+            <div className={"main-container"}>
+              <main className={() => {return}}>
+                <form onSubmit={(e) => e.preventDefault()} className={"container-search-bar"}>
+                  <input type="text" placeholder="Write here your search parameters. E.g.: Mozambique, Portugal" onChange={() => {return}} />
+                  <button className={() => {return}}>search</button>
+                </form>
+                <div className={"container-search-result"}>
+
+                </div>
+              </main>
+              <aside className={() => {return}}>
+
+              </aside>
+            </div>
+        </React.Fragment>
+    )
+}
