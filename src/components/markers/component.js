c9a1bc876b199520ac844e1eba251a5f1228d1c5
Medita_Knorr <Mediimarvel@Hotmail.com>
2021-04-01 22:15:29 +0200

    Finalizado a janela Aside e os Tags de pesquisa.

--- src/components/markers/component.js
+++ src/components/markers/component.js
@@ -1,12 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { X, Hash } from 'react-bootstrap-icons';
 import './component.scss';
 
-export default function ({ id, country}) {
+export default function ({ data, onPress }) {
+const [hashdata, setHashdata] = useState(data);
+useEffect(() => {setHashdata(data)}, [data]);
+
   return (
-    <div className={"keyword-demo"}>
-    <Hash role="img" aria-label="remove" className={"hashtag"} />
-    <span className={"country-name"}>{country}</span>
-    <X id={id} role="img" aria-label="remove" className= {"remove-item"} onClick={() => {return}} />
-  </div>);
+    <React.Fragment>
+      {
+        hashdata.map((data, key) => (
+          <div key={data.id} className={"tagword-demo"}>
+            <Hash role="img" aria-label="remove" className={"hashtag"} />
+            <span className={"country-name"}>{data.country}</span>
+            <X id={data.id} role="img" aria-label="remove" className= {"remove-item"} onClick={(e) => {onPress(data.id)}} />
+          </div>
+          )
+        )
+      }
+    </React.Fragment>
+    )
+
 }
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,14 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import { X, Hash } from 'react-bootstrap-icons';
 import Marker from '../../components/markers/component';
 import '../../styles/global.scss'
 
 export default function Index() {
   const [data, setData] = useState([]);
   const [query, setQuery] = useState([]);
-  const [marker, setMarker] = useState([]);
+  const [marker, setMarker] = useState([
+    { "id": 1, "country": "Mozambique"
+    }, { "id": 2, "country": "Brazil" }]);
 
   useEffect(() => {  }, []);
+
+  const deleteMarker = (key) => {
+     let filtered = marker.filter(mark => {
+       return mark.id != key;
+     });
+     setMarker(filtered);
+   }
+
     return (
         <React.Fragment>
             <header className={"main-header"}>
@@ -18,7 +27,7 @@ export default function Index() {
             <main className={"main-container"}>
 
               <aside className={"aside-content"}>
-                <Marker id={1} country={"Mozambique"} />
+                <Marker data={marker} onPress={deleteMarker} />
               </aside>
 
               <div className={"main-content-block"}>
